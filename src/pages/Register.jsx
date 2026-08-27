ce5f306b35b5f420fdcd3ca7b52a239dadceaa2f
salmanshahidmalik <salmanshahidap@gmail.com>
2026-08-27 20:58:06 +0500

    Major frontend files

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 
 function Register() {
   return (
@@ -80,5 +80,4 @@ function Register() {
     </div>
   );
 }
-
 export default Register;
