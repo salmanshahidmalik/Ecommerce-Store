def53060c1acc4d064e266a497d53c6aa716ebd3
salmanshahidmalik <salmanshahidap@gmail.com>
2026-09-01 19:43:40 +0500

    protected routes 3.0

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,7 +2,9 @@ import { create } from "zustand";
 
 const useAuthStore = create((set) => ({
   isLoggedIn: false,
-  login: () => set({isLoggedIn: true}),
+  login: () => {
+    localStorage.setItem("isLoggedIn", "true");
+    set({isLoggedIn: true})},
 
 }));
 
