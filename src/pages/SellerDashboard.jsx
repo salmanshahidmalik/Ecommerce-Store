52218887254efdea771a26b492cdfdda0838ed8c
salmanshahidmalik <salmanshahidap@gmail.com>
2026-09-01 19:39:28 +0500

    protected routes

--- src/pages/SellerDashboard.jsx
+++ src/pages/SellerDashboard.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
+import useAuthStore from "../store/authStore";
 
+
+
+const login = useAuthStore((state) => state.login);
 function SellerDashboard() {
   return (
     <div className="max-w-7xl mx-auto p-6">
