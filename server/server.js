3bdbaedc0a162b02019b2b217c54ffecfc9eb093
salmanshahidmalik <salmanshahidap@gmail.com>
2026-09-03 02:11:02 +0500

    products from server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 // Import the Express framework
 import express from 'express';
+import products from "../src/data/products.js" 
+import cors from "cors";
 
 // Initialize the Express application
 const app = express();
+app.use(cors());
+app.get("/products", (req, res) => {
+  res.status(200).json(products);
+});
+
 
 // Start the server on port 5000 and log a success message
-app.listen(5000, () => {
-  console.log('Express server started successfully on port 5000!!!');
+app.listen(3000, () => {
+  console.log('Express server started successfully on port 3000!!!');
 });
